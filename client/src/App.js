ac263a6ad010bb1f48bbd41e008a5eb0326240d2
Juan Lacruz <juanluislcm@gmail.com>
2024-07-05 18:38:15 -0400

    chat app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
